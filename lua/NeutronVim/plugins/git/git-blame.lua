714b279b47e27c3001cfe7dd238747030ddf8c1a
daUnknownCoder <takshilmistry@gmail.com>
2023-09-23 17:13:10 +0530

    edited more visual options for git-blame.lua

--- lua/NeutronVim/plugins/git/git-blame.lua
+++ lua/NeutronVim/plugins/git/git-blame.lua
@@ -3,7 +3,8 @@ return {
   config = function()
     require('gitblame').setup {
       enabled = true,
-      vim.cmd [[let g:gitblame_date_format = '%r']]
+      vim.cmd [[let g:gitblame_date_format = '%r']],
+      vim.cmd [[let g:gitblame_message_when_not_committed = "This ain't committed yet, What are u doin... "]],
     }
   end
 }

2cfec624d80a7f04f16fa5b5f416c6566209795e
daUnknownCoder <takshilmistry@gmail.com>
2023-09-23 17:54:10 +0530

    removed empty opts function

--- lua/NeutronVim/plugins/init/flash.lua
+++ lua/NeutronVim/plugins/init/flash.lua
@@ -1,7 +1,6 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {},
   keys = {
     { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },

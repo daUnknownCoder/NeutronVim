7d34d201fc10343dd819157fc108b2b29e2f2b76
daUnknownCoder <takshilmistry@gmail.com>
2023-09-23 17:28:58 +0530

    tweaked gitsigns config

--- lua/NeutronVim/plugins/git/gitsigns.lua
+++ lua/NeutronVim/plugins/git/gitsigns.lua
@@ -11,22 +11,22 @@ return {
         changedelete = { text = '~' },
         untracked    = { text = '┆' },
       },
-      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-      numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-      word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      signcolumn = true,
+      numhl      = true,
+      linehl     = true,
+      word_diff  = false,
       watch_gitdir = {
         follow_files = true
       },
       attach_to_untracked = true,
-      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame = true,
       current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
+        virt_text_pos = 'eol',
+        delay = 100,
         ignore_whitespace = false,
       },
-      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+      current_line_blame_formatter = ' <author>, <author_time:%R> - <summary>',
       sign_priority = 6,
       update_debounce = 100,
       status_formatter = nil, -- Use default

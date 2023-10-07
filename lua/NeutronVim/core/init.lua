5e19a74a819852f5b9e1b6ba57a70a3d50923db8
NeutronVim <takshilmistry@gmail.com>
2023-10-07 21:12:09 +0530

    removed non-required user-defined commands

--- lua/NeutronVim/core/init.lua
+++ lua/NeutronVim/core/init.lua
@@ -1,29 +1,3 @@
--- Reload neovim config
-vim.api.nvim_create_user_command('ReloadConfig', function()
-  for name, _ in pairs(package.loaded) do
-    if name:match('^plugins') then
-      package.loaded[name] = nil
-    end
-  end
-
-  dofile(vim.env.MYVIMRC)
-  vim.notify('NeutronVim configuration reloaded!', vim.log.levels.INFO)
-end, {})
-
--- Copy relative path
-vim.api.nvim_create_user_command('CRpath', function()
-  local path = vim.fn.expand('%')
-  vim.fn.setreg('+', path)
-  vim.notify('Copied "' .. path .. '" to the clipboard!')
-end, {})
-
--- Copy absolute path
-vim.api.nvim_create_user_command('CApath', function()
-  local path = vim.fn.expand('%:p')
-  vim.fn.setreg('+', path)
-  vim.notify('Copied "' .. path .. '" to the clipboard!')
-end, {})
-
 local function augroup(name)
   return vim.api.nvim_create_augroup('NeutronVim' .. name, { clear = true })
 end
@@ -150,4 +124,3 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
   end,
 })
-

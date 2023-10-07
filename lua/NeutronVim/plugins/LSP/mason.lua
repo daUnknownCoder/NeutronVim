91e037b2c9273bb8be77de7be6661511cd6cc4ab
NeutronVim <takshilmistry@gmail.com>
2023-10-07 22:25:34 +0530

    updated events for mason

--- lua/NeutronVim/plugins/LSP/mason.lua
+++ lua/NeutronVim/plugins/LSP/mason.lua
@@ -1,6 +1,6 @@
 return {
   "williamboman/mason.nvim",
-  event = "BufReadPost",
+  event = { "BufReadPost", "BufNewFile" },
   lazy = true,
   dependencies = {
     { "williamboman/mason-lspconfig.nvim", event = "BufRead" },

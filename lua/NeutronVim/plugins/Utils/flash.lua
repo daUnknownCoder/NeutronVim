ee8d4a9d04a5263d49f8337d252a8554aa6d4611
NeutronVim <takshilmistry@gmail.com>
2023-10-20 23:27:11 +0530

    fix: added stylua ignore

--- lua/NeutronVim/plugins/Utils/flash.lua
+++ lua/NeutronVim/plugins/Utils/flash.lua
@@ -1,38 +1,14 @@
 return {
   "folke/flash.nvim",
   lazy = true,
-  keys = {
-    {
-      "<leader>ss",
-      mode = { "n", "o", "x" },
-      function()
-        require("flash").jump()
-      end,
-      desc = "Flash",
+  vscode = true,
+  opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
-    {
-      "<leader>sS",
-      mode = { "n", "o", "x" },
-      function()
-        require("flash").treesitter()
-      end,
-      desc = "Flash Treesitter",
-    },
-    {
-      "<leader>sr",
-      mode = "o",
-      function()
-        require("flash").remote()
-      end,
-      desc = "Remote Flash",
-    },
-    {
-      "<leader>sR",
-      mode = { "o", "x" },
-      function()
-        require("flash").treesitter_search()
-      end,
-      desc = "Treesitter Search",
-    },
-  },
 }

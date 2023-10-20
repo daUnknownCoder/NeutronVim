d4d4355a5abac2851423cd542a7f7f87768456c6
NeutronVim <takshilmistry@gmail.com>
2023-10-20 23:26:21 +0530

    refactor: added max_[height/width] size func

--- lua/NeutronVim/plugins/UI/notify.lua
+++ lua/NeutronVim/plugins/UI/notify.lua
@@ -9,6 +9,18 @@ return {
       desc = "Dismiss all Notifications",
     },
   },
+  opts = {
+    timeout = 3000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
+  },
   lazy = true,
   config = function()
     vim.opt.termguicolors = true
@@ -50,7 +62,6 @@ return {
       "warn",
       "info",
     }
-    ---@diagnostic disable-next-line: duplicate-set-field
     vim.lsp.handlers["window/showMessage"] = function(method, params)
       vim.notify(method.message, severity[params.type])
     end

f0aef06f2724b8bdbd1d2a6aaf67e7ae8b46d099
NeutronVim <takshilmistry@gmail.com>
2023-10-20 23:19:24 +0530

    feat: added formatter and slow formatter functions
    added prettierd to supported ft, added a function to auto detect slow formatters and disable async

--- lua/NeutronVim/plugins/Formatting/formatting.lua
+++ lua/NeutronVim/plugins/Formatting/formatting.lua
@@ -19,12 +19,12 @@ return {
     end
     conform.setup({
       formatters_by_ft = {
-        javascript = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
+        javascript = { "prettier", "prettierd" },
+        css = { "prettier", "prettierd" },
+        html = { "prettier", "prettierd" },
+        json = { "prettier", "prettierd" },
+        yaml = { "prettier", "prettierd" },
+        markdown = { "prettier", "prettierd" },
         lua = { "stylua" },
         python = { "ruff", "black" },
       },
@@ -33,6 +33,28 @@ return {
         async = false,
         timeout_ms = 500,
       },
+    })
+    local slow_format_filetypes = { "markdown" }
+    require("conform").setup({
+      format_on_save = function(bufnr)
+        if slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+        local function on_format(err)
+          if err and err:match("timeout$") then
+            slow_format_filetypes[vim.bo[bufnr].filetype] = true
+          end
+        end
+
+        return { timeout_ms = 200, lsp_fallback = true }, on_format
+      end,
+
+      format_after_save = function(bufnr)
+        if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+        return { lsp_fallback = true }
+      end,
     })
   end,
 }

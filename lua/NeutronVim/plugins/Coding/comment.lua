06f7f0cf16dc2d41c34644461249c5e863f2e203
NeutronVim <takshilmistry@gmail.com>
2023-10-22 00:15:37 +0530

    perf: switched from  to

--- lua/NeutronVim/plugins/Coding/comment.lua
+++ lua/NeutronVim/plugins/Coding/comment.lua
@@ -1,5 +1,13 @@
 return {
   "numToStr/Comment.nvim",
   config = true,
-  event = { "VeryLazy" },
+  keys = {
+    { "gcc", mode = "n", desc = "Comment toggle current line" },
+    { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+    { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+    { "gbc", mode = "n", desc = "Comment toggle current block" },
+    { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+    { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+  },
+  lazy = true,
 }

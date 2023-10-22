077e8106cf419a7d684e1330eb72e9d0e8accc47
NeutronVim <takshilmistry@gmail.com>
2023-10-22 19:22:37 +0530

    refactor: minimap.vim for search highlights

--- lua/NeutronVim/plugins/Utils/minimap.lua
+++ lua/NeutronVim/plugins/Utils/minimap.lua
@@ -5,6 +5,7 @@ return {
   config = function()
     vim.cmd([[let g:minimap_width = 20
               let g:minimap_auto_start = 1
+              let g:minimap_highlight_search = 1
               let g:minimap_auto_start_win_enter = 1]])
   end,
 }

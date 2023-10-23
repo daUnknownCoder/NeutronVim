99d2370376d77106f7e6d7cecf368141e42321aa
NeutronVim <takshilmistry@gmail.com>
2023-10-23 23:57:29 +0530

    🚀 perf(UI): lazy-loading of colorscheme and custom highlights

--- init.lua
+++ init.lua
@@ -1,40 +1,36 @@
-require("NeutronVim.core.opts")
-require("NeutronVim.core.bootstrap")
-require("NeutronVim.core.health")
-require("NeutronVim.core.autocmd")
-local modules = {
-  "NeutronVim.core.remaps",
-  "NeutronVim.core.icons",
-  "NeutronVim.core.global",
-  "NeutronVim.core.log",
-}
-local async
-async = vim.loop.new_async(vim.schedule_wrap(function()
-  for i = 1, #modules, 1 do
-    local ok, res = xpcall(require, debug.traceback, modules[i])
-    if not ok then
-      print("Error loading module : " .. modules[i])
-      print(res)
+vim.defer_fn(function()
+  for _, source in ipairs({
+    "remaps",
+    "autocmd",
+    "icons",
+    "global",
+    "health",
+    "log",
+  }) do
+    local status_ok, fault = pcall(require, "NeutronVim.core." .. source)
+    if not status_ok then
+      vim.api.nvim_err_writeln("Error loading " .. source .. "\n\n" .. fault)
     end
   end
-  async:close()
-end))
-async:send()
+end, 0)
+require("NeutronVim.core.bootstrap")
+require("NeutronVim.core.opts")
+
+vim.cmd([[autocmd VimEnter * colorscheme tokyonight]])
 
 vim.opt.background = "dark"
-vim.cmd([[colorscheme tokyonight]])
 
-vim.cmd([[highlight Keyword gui=italic guifg=#F08753]])
-vim.cmd([[highlight Visual gui=nocombine guifg=#EA3535]])
-vim.cmd([[highlight Comment gui=italic,underline guifg=#a1afaf]])
-vim.cmd([[highlight Normal gui=nocombine guifg=Cyan]])
-vim.cmd([[highlight Number gui=italic guifg=#f7768e]])
-vim.cmd([[highlight String gui=italic guifg=#69ff00]])
-vim.cmd([[highlight @variable gui=nocombine guifg=#bab86c]])
-vim.cmd([[highlight @field gui=italic guifg=#2ac3de]])
-vim.cmd([[highlight @property gui=italic guifg=lightgreen]])
-vim.cmd([[highlight NvimTreeIndentMarker guifg=#61afef]])
-vim.cmd([[highlight Special gui=italic guifg=gold]])
-vim.cmd([[highlight Function gui=italic guifg=orange]])
-vim.cmd([[filetype plugin indent on]])
-vim.cmd([[syntax on]])
+vim.cmd([[autocmd VimEnter * highlight Keyword gui=italic guifg=#F08753]])
+vim.cmd([[autocmd VimEnter * highlight Visual gui=nocombine guifg=#EA3535]])
+vim.cmd([[autocmd VimEnter * highlight Comment gui=italic,underline guifg=#a1afaf]])
+vim.cmd([[autocmd VimEnter * highlight Normal gui=nocombine guifg=Cyan]])
+vim.cmd([[autocmd VimEnter * highlight Number gui=italic guifg=#f7768e]])
+vim.cmd([[autocmd VimEnter * highlight String gui=italic guifg=#69ff00]])
+vim.cmd([[autocmd VimEnter * highlight @variable gui=nocombine guifg=#7fe1aa]])
+vim.cmd([[autocmd VimEnter * highlight @field gui=italic guifg=#2ac3de]])
+vim.cmd([[autocmd VimEnter * highlight @property gui=italic guifg=lightgreen]])
+vim.cmd([[autocmd VimEnter * highlight Special gui=italic guifg=gold]])
+vim.cmd([[autocmd VimEnter * highlight Function gui=italic guifg=orange]])
+vim.cmd([[autocmd VimEnter * highlight NvimTreeIndentMarker guifg=#61afef]])
+vim.cmd([[autocmd VimEnter * filetype plugin indent on]])
+vim.cmd([[autocmd VimEnter * syntax on]])

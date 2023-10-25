ce468da8dff0ecfb0c548469e6046a52850af428
NeutronVim <takshilmistry@gmail.com>
2023-10-25 23:13:33 +0530

    feat(lsp-lens): Added LSP Lens
    LSP-[Definition/References] like JB's Idea

---
+++ lua/NeutronVim/plugins/LSP/lsp-lens.lua
@@ -0,0 +1,13 @@
+return {
+  "VidocqH/lsp-lens.nvim",
+  lazy = true,
+  event = "LspAttach",
+  opts = {
+    enable = true,
+    commit = "13d25ad8bd55aa34cc0aa3082e78a4157c401346",
+    include_declaration = true,
+    sections = {
+      definitions = true,
+    },
+  },
+}

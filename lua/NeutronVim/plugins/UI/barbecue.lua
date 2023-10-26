97f9364d5eb78707249ae2a04a2b21b559f4933f
NeutronVim <takshilmistry@gmail.com>
2023-10-26 16:46:54 +0530

    refactor(barbecue): better barbecue.nvim, updated config

--- lua/NeutronVim/plugins/UI/barbecue.lua
+++ lua/NeutronVim/plugins/UI/barbecue.lua
@@ -8,8 +8,34 @@ return {
   },
   lazy = true,
   event = "LspAttach",
-  opts = {
-    create_autocmd = false,
-    show_modified = true,
-  },
+  config = function()
+    local barbecue_status_ok, barbecue = pcall(require, "barbecue")
+    if not barbecue_status_ok then
+      print("barbecue not found!")
+    end
+    local icons_ok, icons = pcall(require, "NeutronVim.core.icons")
+    if not icons_ok then
+      print("Unable to import icons!")
+    end
+    barbecue.setup({
+      create_autocmd = false,
+      show_modified = true,
+      kinds = icons.kind,
+      symbols = {
+        separator = icons.ui.ArrowClosed,
+      },
+    })
+    vim.api.nvim_create_autocmd({
+      "WinScrolled",
+      "BufWinEnter",
+      "CursorHold",
+      "InsertLeave",
+      "BufModifiedSet",
+    }, {
+      group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+      callback = function()
+        require("barbecue.ui").update()
+      end,
+    })
+  end
 }

5aca1a70bafef34285fe2d93ec64c31225e27ecf
NeutronVim <takshilmistry@gmail.com>
2023-10-26 22:27:06 +0530

    fix(lsp-lens): removed pinning to commit as it is not required

--- lua/NeutronVim/plugins/LSP/lsp-lens.lua
+++ lua/NeutronVim/plugins/LSP/lsp-lens.lua
@@ -4,7 +4,6 @@ return {
   event = "LspAttach",
   opts = {
     enable = true,
-    commit = "13d25ad8bd55aa34cc0aa3082e78a4157c401346",
     include_declaration = true,
     sections = {
       definitions = true,

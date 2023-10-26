135e18918e9f6129ed4486cde6050bc07890a34e
NeutronVim <takshilmistry@gmail.com>
2023-10-26 17:25:04 +0530

    refactor: update keymaps

--- lua/NeutronVim/plugins/Coding/comment.lua
+++ lua/NeutronVim/plugins/Coding/comment.lua
@@ -2,12 +2,12 @@ return {
   "numToStr/Comment.nvim",
   config = true,
   keys = {
-    { "gcc", mode = "n", desc = "Comment toggle current line" },
-    { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-    { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-    { "gbc", mode = "n", desc = "Comment toggle current block" },
-    { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-    { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+    { "gcc", mode = "n",          desc = "Comment toggle current line" },
+    { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+    { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+    { "gbc", mode = "n",          desc = "Comment toggle current block" },
+    { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+    { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
   },
   lazy = true,
 }

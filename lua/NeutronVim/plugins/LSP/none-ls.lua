cd3a6490a267a013ef90d4318db21ed9a8120974
daUnknownCoder <takshilmistry@gmail.com>
2023-12-03 23:02:05 +0530

    refactor: client name edit

--- lua/NeutronVim/plugins/LSP/none-ls.lua
+++ lua/NeutronVim/plugins/LSP/none-ls.lua
@@ -33,7 +33,7 @@ return {
             callback = function()
               vim.lsp.buf.format({
                 filter = function(client)
-                  return client.name == "none-LS"
+                  return client.name == "null-ls"
                 end,
                 bufnr = bufnr,
               })

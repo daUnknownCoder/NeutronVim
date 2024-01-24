1ac17268fbb857b055ef015a7e72ccbe0d6e525f
daUnknownCoder <takshilmistry@gmail.com>
2024-01-24 23:40:22 +0530

    fix(alpha): alpha changes coz some elements went out of screen

--- lua/NeutronVim/plugins/UI/alpha.lua
+++ lua/NeutronVim/plugins/UI/alpha.lua
@@ -31,9 +31,8 @@ return {
       dashboard.button("n", icons.ui.Project .. " File Manager [Nvim-Tree]", ":NvimTreeFindFileToggle <CR>"),
       dashboard.button("N", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("g", icons.kind.Text .. " Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("l", icons.ui.Sleep .. " Lazy", ":Lazy<CR>"),
-      dashboard.button("L", icons.git.Branch .. "  LazyGit", ":LazyGit<CR>"),
-      dashboard.button("q", icons.ui.SignIn .. " Quit", ":qa<CR>"),
+      dashboard.button("l", icons.ui.Sleep .. " Lazy", ":Lazy <CR>"),
+      dashboard.button("q", icons.ui.SignIn .. " Quit", ":qa <CR>"),
     }
     for _, button in ipairs(dashboard.section.buttons.val) do
       button.opts.hl = "AlphaButtons"
@@ -78,7 +77,14 @@ return {
           .. vim.version().minor
           .. "."
           .. vim.version().patch
-        local plugins = "NeutronVim has loaded " .. stats.count .. " plugins in " .. icons.ui.Electric .. ms .. "ms"
+        local plugins = "NeutronVim has loaded "
+          .. stats.loaded
+          .. "/"
+          .. stats.count
+          .. " plugins in "
+          .. icons.ui.Electric
+          .. ms
+          .. "ms"
         local footer = version .. " -> " .. plugins
         dashboard.section.footer.val = footer
         pcall(vim.cmd.AlphaRedraw)

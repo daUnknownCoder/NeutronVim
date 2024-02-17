456ce94284492b94d11cd1c62c3d48a93ebdf799
daUnknownCoder <takshilmistry@gmail.com>
2024-02-17 19:58:34 +0530

    refactor(reformat): update formatting

--- lua/NeutronVim/plugins/Coding/comment.lua
+++ lua/NeutronVim/plugins/Coding/comment.lua
@@ -1,13 +1,21 @@
 return {
   "numToStr/Comment.nvim",
-  config = true,
   keys = {
-    { "gcc", mode = "n",          desc = "Comment toggle current line" },
-    { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
-    { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
-    { "gbc", mode = "n",          desc = "Comment toggle current block" },
-    { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-    { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
+    { "gcc", mode = "n", desc = "Comment toggle current line" },
+    { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+    { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+    { "gbc", mode = "n", desc = "Comment toggle current block" },
+    { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+    { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
   },
   lazy = true,
+  config = function()
+    local comment_status_ok, comment = pcall(require, "Comment")
+    if not comment_status_ok then
+      print("Comment not found!")
+    end
+    comment.setup({
+      ignore = "^$",
+    })
+  end,
 }

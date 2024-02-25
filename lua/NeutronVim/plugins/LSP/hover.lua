1b215eb93ae8e997eca20b551b99e0713fdb4a94
daUnknownCoder <takshilmistry@gmail.com>
2024-02-26 00:39:16 +0530

    update hover.lua

--- lua/NeutronVim/plugins/LSP/hover.lua
+++ lua/NeutronVim/plugins/LSP/hover.lua
@@ -2,10 +2,14 @@ return {
   "lewis6991/hover.nvim",
   lazy = true,
   keys = {
-    { "K", "<cmd>lua require('hover').hover()<CR>", desc = "Hover" },
-    { " k", "<cmd>lua require('hover').hover_select()<CR>", desc = "Hover Select" },
+    { "\\h", "<cmd>lua require('hover').hover()<CR>", desc = "Hover" },
+    { "\\H", "<cmd>lua require('hover').hover_select()<CR>", desc = "Hover Select" },
+    { "\\kj", "<cmd>lua require('hover').hover_switch('previous')<CR>", desc = "Hover (Previous Source)" },
+    { "\\kk", "<cmd>lua require('hover').hover_switch('next')<CR>", desc = "Hover (Next Source)" },
+    { "<MouseMove>", "<cmd>lua require('hover').hover_mouse()<CR>", desc = "Hover (Mouse)" },
   },
   config = function()
+    vim.o.mousemoveevent = true
     local hover_status_ok, hover = pcall(require, "hover")
     if not hover_status_ok then
       print("hover not found!")
@@ -15,7 +19,6 @@ return {
         require("hover.providers.lsp")
         require("hover.providers.gh")
         require("hover.providers.gh_user")
-        require("hover.providers.jira")
         require("hover.providers.man")
       end,
       preview_opts = {
@@ -23,6 +26,10 @@ return {
       },
       preview_window = true,
       title = true,
+      mouse_providers = {
+        "LSP",
+      },
+      mouse_delay = 1000,
     })
   end,
 }
